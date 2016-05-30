851e4bf522d1a819955343212c4db207868e6aff
straysheep3 <getloose.kage@gmail.com>
2016-05-31 00:18:13 +0900

    created articles show page

--- Gemfile
+++ Gemfile
@@ -9,6 +9,10 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'haml', '~> 4.0', '>= 4.0.7'
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
+gem 'simple_form', '~> 3.2', '>= 3.2.1'
+gem 'devise', '~> 4.1', '>= 4.1.1'
 
 group :development, :test do
   gem 'byebug'
---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    #code
+  end
+
+  def show
+    #code
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    #code
+  end
+
+  def update
+    #code
+  end
+
+  def destroy
+    #code
+  end
+
+  private
+
+    def find_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:content, :title)
+    end
+
+end
